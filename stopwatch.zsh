a90f08af52bfdcb89df8718225bcc86e1e7ffc21
Ivan Smirnov <ifsmirnov@yandex.ru>
2024-06-19 15:28:16 +0300

    [stopwatch] Disable notifications

--- stopwatch.zsh
+++ stopwatch.zsh
@@ -21,9 +21,9 @@ function stopwatch_post() {
             last_command=$(echo $history[$((HISTCMD-1))] | cut -d' ' -f1)
             RPROMPT="[$last_command: $((msec_delta/1000))s $((msec_delta%1000))ms] $RPROMPT"
 
-            if [[ $msec_delta -gt 10000 ]] && which notify-send >/dev/null; then
-                msg "'$history[$((HISTCMD-1))]' finished in $((msec_delta/1000)) s"
-            fi
+            # if [[ $msec_delta -gt 10000 ]] && which notify-send >/dev/null; then
+            #     msg "'$history[$((HISTCMD-1))]' finished in $((msec_delta/1000)) s"
+            # fi
         else
             RPROMPT="$SAVED_RPROMPT"
         fi
